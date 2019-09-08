f099f8d90ccbe7f2779f3873890e2e400f9dc6c3
josephttran <42635012+josephttran@users.noreply.github.com>
2019-09-08 16:19:22 -0700

    Reduce measured time about 50%

--- 2019-September/oddly-reversible.py
+++ 2019-September/oddly-reversible.py
@@ -8,19 +8,20 @@ def isVeryOdd(n):
 
 def getTotal(n):
     total = 0
-    for i in range(0, n, 1):
+    for i in range(0, n, 2):
         if (i % 10 == 0):
             continue
+
         reversed = str(i)[::-1]
+        intReversed = int(reversed)
 
-        result = (i + int(reversed))
-        if (result % 2 == 0):
-            continue
+        if intReversed % 2 == 1:
+            result = (i + intReversed)
 
-        if isVeryOdd(result):
-            total += 1
+            if isVeryOdd(result):
+                total += 1
 
-    return total
+    return total * 2
 
 start = time.time()
 total = getTotal(10000000)
