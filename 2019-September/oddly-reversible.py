496637e0fe278f1078a89f4de6993e4a5cf7c283
josephttran <42635012+josephttran@users.noreply.github.com>
2019-09-11 23:39:29 -0700

    Make program faster

--- 2019-September/oddly-reversible.py
+++ 2019-September/oddly-reversible.py
@@ -1,21 +1,26 @@
 import time
 
 def isVeryOdd(n):
-    for i in str(n):
-        if (int(i) % 2 == 0):
+    while n > 9:
+        n = n // 10
+        if n % 2 == 0:
             return False
+
     return True
 
 def getTotal(n):
     total = 0
+
     for i in range(0, n, 2):
         if (i % 10 == 0):
             continue
+        
+        numString = str(i)
 
-        reversed = str(i)[::-1]
-        intReversed = int(reversed)
+        if numString[0] in ['1', '3', '5', '7', '9']:
+            reversed = numString[::-1]
+            intReversed = int(reversed)
 
-        if intReversed % 2 == 1:
             result = (i + intReversed)
 
             if isVeryOdd(result):
